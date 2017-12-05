b8a76042673743fa67e6c79a8876caa72b0bc375
ramesh-rams <ramesh3119@gmail.com>
2017-12-05 20:07:14 +0530

    addded file

---
+++ main/test.java
@@ -0,0 +1,21 @@
+public class test()
+
+bool isInteger;
+public static void main(string[] args)
+
+{
+  int num;
+ public bool isEvenNuber()
+ {
+  if(num%2)
+  	{
+	isInteger=true;
+	}
+  else
+  	{
+  	 isInteger=false;	
+	}	
+   return isInteger;
+ }	
+
+}
